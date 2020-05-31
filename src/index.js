85bdce2da4ccbe43483f41dc409806e8ae220c2f
lxdi <alexfordeal@gmail.com>
2020-06-01 01:19:56 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = Object.assign(
+    {},
+    require('./drag-utils.js'),
+    require('./draggable-tree-utils.js'),
+    require('./import-utils.js'),
+    require('./linked-list.js'),
+    require('./tree-utils.js'),
+);
